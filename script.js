9390a57b5d26f9f44223cf0d69926780236bce7b
lfoalves <lfoalves.dev@gmail.com>
2022-05-30 23:25:12 -0400

    ajustes no modal e no script

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const modalTitle = document.querySelector('#modal h2');
+const inputModal = document.querySelector('.input-modal');
+console.log(inputModal);
 
 revert.onclick = () => {
     modal.classList.add('open');
-    modalTitle.innerHTML = 'Deseja desfazer o último registro?'
+    modalTitle.innerHTML = 'Deseja desfazer o último registro?';
+    inputModal.style.display = 'none';
 }
 
 set.onclick = () => {
     modal.classList.add('open');
     modalTitle.innerHTML = 'Deseja alterar a medida padrão?'
+    inputModal.style.display = 'flex';
 }
 
 water.onclick = () => {
