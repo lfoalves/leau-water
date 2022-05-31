c5a9ef68e9d1f9bf66a4e8eaefe29de20dbfe906
lfoalves <lfoalves.dev@gmail.com>
2022-05-30 23:50:28 -0400

    ajustes no modal e design

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const modalTitle = document.querySelector('#modal h2');
 const inputModal = document.querySelector('.input-modal');
 console.log(inputModal);
 
+// buttons prevenção de comportamento padrão
+cCancel.addEventListener('click', function(e) {
+    e.preventDefault();
+})
+cConfirm.addEventListener('click', function(e) {
+    e.preventDefault();
+})
+
 revert.onclick = () => {
     modal.classList.add('open');
     modalTitle.innerHTML = 'Deseja desfazer o último registro?';
     inputModal.style.display = 'none';
 }
 
-set.onclick = () => {
+set.onclick = (e) => {
+    e.preventDefault();
     modal.classList.add('open');
     modalTitle.innerHTML = 'Deseja alterar a medida padrão?'
     inputModal.style.display = 'flex';
@@ -19,6 +28,5 @@ water.onclick = () => {
 }
 
 cCancel.onclick = () => {
-    modal.classList.remove('open');
-    
+    modal.classList.remove('open');    
 }
